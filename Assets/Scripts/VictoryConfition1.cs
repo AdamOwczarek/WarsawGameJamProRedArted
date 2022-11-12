cde44c7b9e41d25c21668b0725feb70c9e41a51f
Adam Owczarek <adam.piotr.khan@gmail.com>
2022-11-12 17:29:53 +0100

    add victory screens and colisions that enable them

--- Assets/Scripts/VictoryConfition1.cs
+++ Assets/Scripts/VictoryConfition1.cs
@@ -8,11 +8,16 @@ public class VictoryConfition1 : MonoBehaviour
 {
     [SerializeField] Collider collider;
     [SerializeField] private GameObject victoryPanel;
+    [SerializeField] private GameObject victoryPanel2;
+    [SerializeField] private GameObject victoryPanel3;
     private void OnTriggerEnter(Collider collider)
     {
-        if (collider.tag == "Player2")
+        if (collider.CompareTag("Player2"))
         {
             victoryPanel.SetActive(true);
+            victoryPanel2.SetActive(true);
+            victoryPanel3.SetActive(true);
+           
         }
        
         
