afc77a22d59ae270227b6b3a864de241cc9352c2
Cyngiel <cyngiel@users.noreply.github.com>
2022-11-13 02:20:44 +0100

    konfetti

--- Assets/Scripts/VictoryConfition.cs
+++ Assets/Scripts/VictoryConfition.cs
@@ -11,6 +11,7 @@ public class VictoryConfition : MonoBehaviour
     [SerializeField] private GameObject victoryPanel2;
     [SerializeField] private GameObject victoryPanel3;
     [SerializeField] private GameObject disactivate;
+    public GameObject konfetti;
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.CompareTag("Player1"))
@@ -19,6 +20,8 @@ public class VictoryConfition : MonoBehaviour
             victoryPanel2.SetActive(true);
             victoryPanel3.SetActive(true);
             disactivate.SetActive(false);
+            konfetti.SetActive(false);
+            konfetti.SetActive(true);
         }
        
         
